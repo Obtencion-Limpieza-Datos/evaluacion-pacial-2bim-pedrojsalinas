7056c02d194c956469608912a65b3082aff1b80a
Pedro Salinas <pedrosalinas5@gmail.com>
2018-09-11 16:10:12 -0500

    correcion

--- alcaldes/alcaldes/spiders/partido.py
+++ alcaldes/alcaldes/spiders/partido.py
@@ -8,7 +8,9 @@ class QuotesSpider(scrapy.Spider):
 
     def parse(self, response):
         for l in response.css('.mw-parser-output'):
+            cantones =l.xpath('//table[@class = "wikitable"]/tbody/tr/td[1]/a/text()').extract()
             partidos = l.xpath('//table[@class = "wikitable"]/tbody/tr/td[3]/a/text()').extract()
+            nombres = l.xpath('//table[@class = "wikitable"]/tbody/tr/td[2]/text()').extract()
             for partido in partidos:
                 yield {
                     'partido': partido
